1f8be92251a28fb758f89935bf301c62d99e1e4b
Richard Okoro <glasshouse747@gmail.com>
2025-06-28 08:15:47 +0100

    roboshop commit

--- tools-setup/variables.tf
+++ tools-setup/variables.tf
@@ -6,9 +6,9 @@ variable "tools" {
     workstation = {
       vm_size = "Standard_B2als_v2"
     }
-    github-runner = {
-      vm_size = "Standard_B2als_v2"
-    }
+    # github-runner = {
+    #   vm_size = "Standard_B2als_v2"
+    # }
     # elk-stack = {
     #   vm_size = "Standard_E2bds_v5"
     #   spot    = true
